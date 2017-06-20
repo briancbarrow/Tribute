d6b781830a74eab7f8ed8ddd1775d4cb47021805
Brian Barrow <briancbarrow@gmail.com>
2017-06-20 16:23:06 -0600

    Added tributes controller

---
+++ Tribute/Controllers/TributesController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tribute.Controllers
+{
+    public class TributesController : Controller
+    {
+
+    }
+}
