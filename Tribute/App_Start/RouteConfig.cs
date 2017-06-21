3423fb4ff3cbb90ff283af189fdab3f1d4c6839b
Brian Barrow <briancbarrow@gmail.com>
2017-06-21 15:43:46 -0600

    Updated Default route

--- Tribute/App_Start/RouteConfig.cs
+++ Tribute/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tribute
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Tributes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
