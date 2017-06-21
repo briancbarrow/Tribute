c0bc0d33392be1f2df81095c0acd0fac605a7f2d
Brian Barrow <briancbarrow@gmail.com>
2017-06-21 11:37:05 -0600

    Added Models for Date and Tribute

---
+++ Tribute/Models/Date.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tribute.Models
+{
+    public class Date
+    {
+        public string Year { get; set; }
+        public string Event { get; set; }
+    }
+}
---
+++ Tribute/Models/Tribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tribute.Models
+{
+    public class Tribute
+    {
+        public int Id { get; set; }
+        public string TributeTitle { get; set; }
+        public string BioDescriptionHtml { get; set; }
+        public string ImageLink { get; set; }
+        public string ExternalLink { get; set; }
+        public Date[] BioDates { get; set; }
+    }
+}
